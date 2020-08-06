ae27eb9138744ab5649d52d91a887668e9d8791b
jjc <1127986703@qq.com>
2020-08-06 19:53:22 +0800

    添加js和css，修改字体显示

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const ks = ['Xin Yang', 'Yang Xin', '杨鑫']
+const styles = [
+  {
+    fontSize: '14px',
+  },
+  {
+    fontSize: '16px',
+    color: '#006699',
+    fontWeight: 'bold',
+  },
+  {
+    fontSize: '14px',
+    color: 'rgb(192,0,0)',
+    fontStyle: 'oblique',
+    fontWeight: 'bold',
+  },
+]
